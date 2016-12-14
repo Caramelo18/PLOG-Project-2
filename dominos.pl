e1d5e89d740483a6168234df6dc8ee9bec921a60
Caramelo18 <fabiocaramelo18@gmail.com>
2016-12-14 19:37:59 +0000

    generating pieces

---
+++ dominos.pl
@@ -0,0 +1,17 @@
+:-use_module(library(clpfd)).
+:-use_module(library(lists)).
+
+piece(line, col).
+
+generate_pieces(Max, Output):- generate_pieces(0, Max, [], Output).
+
+generate_pieces(Col, Max, Input, Output):- Col1 is Col + 1, Col1 =< Max,
+                                           generate_piecesRow(0, Col, Input, Intermediate),
+                                           generate_pieces(Col1, Max, Intermediate, Output).
+generate_pieces(Col, Col, List, Output):- generate_piecesRow(0, Col, List, Output), write(Output).
+
+generate_piecesRow(Current, Col, Input, Output):- Curr1 is (Current + 1), Current =< Col,
+                                                  append(Input, [piece(Current, Col)], Intermediate),
+                                                  generate_piecesRow(Curr1, Col, Intermediate, Output).
+
+generate_piecesRow(_, _, List, List).
