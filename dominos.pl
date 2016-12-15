9687d0f8788f7fd1002237076904aa6129e9990c
Caramelo18 <fabiocaramelo18@gmail.com>
2016-12-15 19:51:14 +0000

    finding valid places

--- dominos.pl
+++ dominos.pl
@@ -4,7 +4,7 @@
 :-include('table.pl').
 
 piece(line, col).
-validPlacement(piece(line, col), pline, pcol, pdir).
+validPlacement(piece(A, B), Line, Col, Dir).
 
 
 generate_pieces(Max, Output):- generate_pieces(0, Max, [], Output).
@@ -12,7 +12,7 @@ generate_pieces(Max, Output):- generate_pieces(0, Max, [], Output).
 generate_pieces(Col, Max, Input, Output):- Col1 is Col + 1, Col1 =< Max,
                                            generate_piecesRow(0, Col, Input, Intermediate),
                                            generate_pieces(Col1, Max, Intermediate, Output).
-generate_pieces(Col, Col, List, Output):- generate_piecesRow(0, Col, List, Output), write(Output).
+generate_pieces(Col, Col, List, Output):- generate_piecesRow(0, Col, List, Output).
 
 generate_piecesRow(Current, Col, Input, Output):- Curr1 is (Current + 1), Current =< Col,
                                                   append(Input, [piece(Current, Col)], Intermediate),
@@ -24,4 +24,24 @@ generate_piecesRow(_, _, List, List).
 getRow(Board, Row, Element):- nth0(Row, Board, Element).
 getCol(Row, Col, Element):- nth0(Col, Row, Element).
 
-getElement(Row, Col, Element):- table(Board), getRow(Board, Row, ERow), getCol(ERow, Col, Element). %TODO - change Board?
+getElement(Row, Col, Element):- table1(Board), getRow(Board, Row, ERow), getCol(ERow, Col, Element). %TODO - change Board?
+/*
+getValidPlacementVertical(piece(A,B), Places):-
+    validPlacement(piece(A,B), Line, Column, 0),
+    write('Vaild'),
+    domain([Line], 0, 7),
+    domain([Column], 0, 15),
+    write(Line), write(' '), write(Col), write('\n'),
+    getElement(Line, Column, A), Line1 is Line + 1, getElement(Line1, Column, B), %VERTICAl
+    labeling([], Places).*/
+
+isValidPlacement(Line, Col, piece(A,B), Dir):- getElement(Line, Col, A), ((Line1 is Line+1, getElement(Line1, Col, B), Dir is 0);
+                                                                     ((Col1 is Col+1), getElement(Line, Col1, B), Dir is 1)).
+
+listValidPlacements(piece(A,B), Output):- findall([piece(A,B), Line, Col, Dir], isValidPlacement(Line, Col, piece(A,B), Dir), Output).
+
+listAllPlacements([Pieces|PiecesS], Input, Output):- listValidPlacements(Pieces, Intermediate), append(Input, Intermediate, Intermediate2),
+                                                     listAllPlacements(PiecesS, Intermediate2, Output).
+listAllPlacements([], I, O):- sort(I, O).
+
+listAllPlacements(Placements):- generate_pieces(4, Pieces), listAllPlacements(Pieces, [], Placements), write(Placements).
