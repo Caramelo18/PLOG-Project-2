1175659884ca12c92094aa34ae25cc4de8520629
Caramelo18 <fabiocaramelo18@gmail.com>
2016-12-21 21:31:59 +0000

    failed attempt to solve a domino

--- dominos.pl
+++ dominos.pl
@@ -1,5 +1,6 @@
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
+:-use_module(library(sets)).
 
 :-include('table.pl').
 
@@ -25,18 +26,9 @@ getRow(Board, Row, Element):- nth0(Row, Board, Element).
 getCol(Row, Col, Element):- nth0(Col, Row, Element).
 
 getElement(Row, Col, Element):- table1(Board), getRow(Board, Row, ERow), getCol(ERow, Col, Element). %TODO - change Board?
-/*
-getValidPlacementVertical(piece(A,B), Places):-
-    validPlacement(piece(A,B), Line, Column, 0),
-    write('Vaild'),
-    domain([Line], 0, 7),
-    domain([Column], 0, 15),
-    write(Line), write(' '), write(Col), write('\n'),
-    getElement(Line, Column, A), Line1 is Line + 1, getElement(Line1, Column, B), %VERTICAl
-    labeling([], Places).*/
 
-isValidPlacement(Line, Col, piece(A,B), Dir):- getElement(Line, Col, A), ((Line1 is Line+1, getElement(Line1, Col, B), Dir is 0);
-                                                                         ((Col1 is Col+1), getElement(Line, Col1, B), Dir is 1)).
+isValidPlacement(Line, Col, piece(A,B), Dir):- getElement(Line, Col, A), ((Line1 is Line+1, getElement(Line1, Col, B), Dir is 0); %Vertical
+                                                                         ((Col1 is Col+1), getElement(Line, Col1, B), Dir is 1)). %Horizontal
 
 listValidPlacements(piece(A,B), Output):- findall(Line-Col-Dir, isValidPlacement(Line, Col, piece(A,B), Dir), Output).
 
@@ -45,17 +37,19 @@ listPlacement([Pieces|PiecesS], [Intermediate|I]):- listValidPlacements(Pieces, 
 listPlacement([], []).
 
 
-listAllPlacements(Placements,Pieces):-  listAllPlacement(Pieces,Placements),!.
+listAllPlacements(Placements,Pieces):-  listPlacement(Pieces,Placements),!.% write(Pieces), write('\n').%, write(Placements).
 
 
-bindSolution(L,C,D,[]):- L #= -1, C #= -1, D #= 2.
+bindSolution(_,_,_,[]).
+
 bindSolution(L,C,D,S):- member(L-C-D,S).
-    
 
 
-validPlace([],[],[],_):-write('end').
+validPlace(_,_,_, [_]).
 validPlace([L1|Ls],[C1|Cs],[D1|Ds],[S1|Ss]):-
                 bindSolution(L1,C1,D1,S1),
+                %write('\n'), write(S1), write(L1), write(' '), write(C1), write(' '), write(D1),
+                %write(Ss), write('\n'),
                 validPlace(Ls,Cs,Ds,Ss).
 
 
@@ -64,16 +58,26 @@ resolveDomino(Width,Height,N,Pieces):-
     length(Col,N),
     length(Direction,N),
 
-    domain(Line,-1,Width),
-    domain(Col,-1,Height),
-    domain(Direction,0,2),
+    domain(Line,0,Width),
+    domain(Col,0,Height),
+    domain(Direction,0,1),
 
-    listAllPlacements(Places,Pieces),
+    listAllPlacements(IPlaces,Pieces),
+    delete(IPlaces, [], Places),
+
+    write(Places), write('\n'), write(Pieces),
     validPlace(Line,Col,Direction,Places),
+    something(Line, Col, Direction, [], X),
+    /*write(X),
+    write('\n'),*/
+    is_set(X),
+    write(X),
 
     labeling([],Line),
     labeling([],Col),
     labeling([],Direction),
+    write('\n'),
+    write(Pieces),
     write('\n'),
     write(Line),
     write('\n'),
@@ -81,9 +85,7 @@ resolveDomino(Width,Height,N,Pieces):-
     write('\n'),
     write(Direction).
 
-
-
-test:-    generate_pieces(8, Pieces),length(Pieces,N), resolveDomino(8,8,N,Pieces).
+test:-    generate_pieces(4, Pieces), length(Pieces,N), write(N), write('\n'), !, resolveDomino(6,5,14,Pieces).
 
 
 test2:- generate_pieces(8, Pieces), write(Pieces),
@@ -92,3 +94,11 @@ test2:- generate_pieces(8, Pieces), write(Pieces),
 
 printa([],[]).
 printa([P|Ps],[S|Ss]):-write(P),write('   '), write(S), write('\n'), printa(Ps,Ss).
+
+something([], [], [], L, L).
+something([Line|LineS], [Col|ColS], [Dir|DirS], Input, Output):- append(Input, [place(Line, Col)], Intermediate),
+                                                                 attach(Line, Col, Dir, Intermediate, Intermediate1),
+                                                                 something(LineS, ColS, DirS, Intermediate1, Output).
+
+attach(Line, Col, 0, Input, Output):- Line1 is Line + 1, append(Input, [place(Line1, Col)], Output).
+attach(Line, Col, 1, Input, Output):- Col1 is Col + 1, append(Input, [place(Line, Col1)], Output).
